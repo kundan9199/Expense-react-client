db852e5ed74cebb7592782d17ecfe7694f22a01d
Aditya Solge <aditya.solge001@umb.edu>
2026-01-28 11:39:11 +0530

    React examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import Student from "./pages/examples/Student";
+import Student1 from "./pages/examples/Student1";
+import Student2 from "./pages/examples/Student2";
+
 function App() {
     return (
         <>
             <h1>Welcome to Expense App</h1>
+            <Student />
+            
+            <Student1 />
+            
+            <Student1 name="John" rollNumber={20} />
+
+            <Student2 name="Sam" rollNumber={40} />
         </>
     );
 }
